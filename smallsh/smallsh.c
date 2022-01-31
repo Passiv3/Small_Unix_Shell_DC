473969510ee0f86232682ed20d2f0270225b06d9
Danny C <76886510+Passiv3@users.noreply.github.com>
2022-01-31 01:05:28 -0800

    Good progress, organized input into a structure, which keeps track of the command, arguments, input or output files, and background flag

--- smallsh/smallsh.c
+++ smallsh/smallsh.c
@@ -2,17 +2,28 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <string.h>
 
 /* I did not plagiarize off of this, but this really helped me
 * get started on this assignment:
 * https://brennan.io/2015/01/16/write-a-shell-in-c/
-* After it got me started, I closed it because I felt like it was cheating,
-* but I got the general layout and function structure from it
+* I got the idea of the general program layout from this link
+* things like having the do while loop
+* and building the readLine, getArgs, and execution lines
+* separate
 */
+
+struct entry {
+	char* command;
+	char* arguments;
+	char* redirectedIn;
+	char* redirectedOut;
+	int background;
+};
 
 /* readLine function
 * gets user input
-* returns pointer to input
+* returns a pointer to input
 */
 char* readLine(void) {
 	char* input = NULL;
@@ -23,23 +34,72 @@ char* readLine(void) {
 	return input;
 }
 
-/* function getArgs
+/* function buildEntry
 * takes user input as an argument
 * tokenizes into an array of arguments
 * returns array
 */
-char getArgs(char* userIn) {
-	char* arguments[sizeof(userIn)];
-	char* token;
+struct entry* buildEntry(char* userIn) {
+	struct entry* currEntry = malloc(sizeof(struct entry));
+	size_t newSpace;
+	int flag = 0;		// flag 0 = initial, 1 = arguments, 2 = input, 3 = output, 4 = ampersand check
 
-	token = strtok_r(userIn, " ");
+	char* saveptr;
+	char* token = strtok_r(userIn, " ", &saveptr);
+	while (token != NULL) {
 
+		if (strcmp(token, "<") == 0) {
+			flag = 2;
+			token = strtok_r(NULL, " ", &saveptr);
+		}
+		else if (strcmp(token, ">") == 0) {
+			flag = 3;
+			token = strtok_r(NULL, " ", &saveptr);
+		}
+		switch (flag) {
+		case 0:
+			currEntry->command = calloc(strlen(token) + 1, sizeof(char));
+			strcpy(currEntry->command, token);
+			flag = 1;
+			break;
+		case 1:
+			if (currEntry->arguments != NULL){
+				newSpace = sizeof(currEntry->arguments) + sizeof(token) + 2;
+				currEntry->arguments = realloc(currEntry->arguments, newSpace);
+				strcat(currEntry->arguments, " ");
+				strcat(currEntry->arguments, token);
+			}
+			else {
+				currEntry->arguments = calloc(strlen(token) + 1, sizeof(char));
+				strcpy(currEntry->arguments, token);
+			}
+			break;
+		case 2:
+			currEntry->redirectedIn = calloc(strlen(token) + 1, sizeof(char));
+			strcpy(currEntry->redirectedIn, token);
+			flag = 4;
+			break;
+		case 3:
+			currEntry->redirectedOut = calloc(strlen(token) + 1, sizeof(char));
+			strcpy(currEntry->redirectedOut, token);
+			flag = 4;
+			break;
+		case 4:
+			if (strcmp(token, "&\n") == 0) {
+				currEntry->background = 1;
+			}
+			break;
+		}
+		token = strtok_r(NULL, " ", &saveptr);
+	}
+	
+	return currEntry;
 }
 
 /* function execute
 * 
 */
-int execute(void) {
+int execute(struct entry* argument) {
 	
 	return 0;
 }
@@ -48,14 +108,15 @@ int execute(void) {
 */
 int main(int argc, char* argv[]) {
 	char* userInput;
-	char* arguments;
-	int status;
+	int again;
 
 	// main loop, continues until status
 //	do {
 		printf(": ");
 		userInput = readLine();
-		arguments = getArgs(userInput);
+		struct entry* arguments = buildEntry(userInput);
+		again = execute(arguments);
+
 		free(userInput);
 
 //	} while (status);
